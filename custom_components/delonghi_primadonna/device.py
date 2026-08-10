c6a2dfc2f58e3ac16dc64e80aaeb2c8cbfd1b065
Chris Langenberg <speedmaze@gmail.com>
2026-08-10 08:23:55 +0200

    refactor: centralize default name constant and use info log for fallback

--- custom_components/delonghi_primadonna/device.py
+++ custom_components/delonghi_primadonna/device.py
@@ -30,7 +30,7 @@ from .const import (AMERICANO_OFF, AMERICANO_ON, AVAILABLE_PROFILES,
                     COFFEE_GROUNDS_CONTAINER_CLEAN,
                     COFFEE_GROUNDS_CONTAINER_DETACHED,
                     COFFEE_GROUNDS_CONTAINER_FULL, CONTROLL_CHARACTERISTIC,
-                    DEBUG, DEFAULT_IMAGE_URL, DEVICE_READY, DEVICE_STATUS,
+                    DEBUG, DEFAULT_DEVICE_NAME, DEFAULT_IMAGE_URL, DEVICE_READY, DEVICE_STATUS,
                     DEVICE_TURNOFF, DOMAIN, DOPPIO_OFF, DOPPIO_ON,
                     ESPRESSO2_OFF, ESPRESSO2_ON, ESPRESSO_OFF, ESPRESSO_ON,
                     HOTWATER_OFF, HOTWATER_ON, LONG_OFF, LONG_ON,
@@ -720,8 +720,8 @@ class DelongiPrimadonna:
                         )
                     ).decode('utf-8')
                 except BleakError as error:
-                    _LOGGER.debug('Could not read NAME_CHARACTERISTIC: %s', error)
-                    self.hostname = self.name or "DeLonghi PrimaDonna"
+                    _LOGGER.info('Could not read NAME_CHARACTERISTIC: %s', error)
+                    self.hostname = self.name or DEFAULT_DEVICE_NAME
                 await self._client.write_gatt_char(
                     uuid.UUID(CONTROLL_CHARACTERISTIC), bytearray(DEBUG)
                 )
